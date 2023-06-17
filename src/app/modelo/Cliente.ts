2c4e9c8bd8000a80ffbadbc67a7a9522816f0f4f
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-06-17 08:03:14 -0300

    Formulario Angular conectando ao Spring e mysql!

---
+++ src/app/modelo/Cliente.ts
@@ -0,0 +1,6 @@
+export class Cliente{
+  id:number = 0;
+  nome:String = '';
+  idade:number = 0;
+  cidade:string = '';
+}
---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent {
+
+  cliente = new Cliente();
+  btnCadastro:boolean = true;
+  tabela:boolean = true;
+
+  constructor(private servico:ClienteService){
+
+  }
+  clientes:Cliente[] = [];
+  selecionar():void{
+    this.servico.selecionar()
+    .subscribe(retorno => this.clientes = retorno);
+  }
+  cadastrar():void{
+
+    this.servico.cadastrar(this.cliente)
+    .subscribe(retorno => {this.clientes.push(retorno)
+    this.cliente = new Cliente();
+    alert('Cliente cadastrado com sucesso !')
+
+    });
+  }
+
+  editar():void{
+    this.servico.editar(this.cliente). subscribe(retorno => {
+      let posicao = this.clientes.findIndex(obj => {
+        return obj.id == retorno.id;
+      });
+      this.clientes[posicao]= retorno;
+      this.cliente = new Cliente();
+      this.btnCadastro = true;
+      this.tabela = true;
+      alert("Cliente alterado com sucesso!")
+    })
+  }
+
+  remover():void{
+    this.servico.remover(this.cliente.id). subscribe(retorno => {
+      let posicao = this.clientes.findIndex(obj => {
+        return obj.id == this.cliente.id
+      });
+      this.clientes.splice(posicao,1);
+      this.cliente = new Cliente();
+      this.btnCadastro = true;
+      this.tabela = true;
+      alert("Cliente removido com sucesso!")
+    })
+  }
+
+  selecionarCliente(posicao:number):void{
+    this.cliente = this.clientes[posicao];
+    this.btnCadastro = false;
+    this.tabela = false;
+  }
+
+  cancelar():void{
+    this.cliente = new Cliente();
+    this.btnCadastro = true;
+    this.tabela = true;
+  }
+  ngOnInit(){
+    this.selecionar();
+  }
+
+
+}
